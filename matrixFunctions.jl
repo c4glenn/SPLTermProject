823efba9e44d803188e8eb580ecde8eed752a7c4
Philip Smith <philip@randomsmiths.com>
2023-11-07 15:13:38 -0500

    create scaffold functions for the featureset we want

---
+++ matrixFunctions.jl
@@ -0,0 +1,38 @@
+# https://docs.julialang.org/en/v1/stdlib/LinearAlgebra/
+
+function dimensions(a)
+    throw("unimplemented")
+end
+
+function transpose(a)
+    throw("unimplemented")
+end
+
+
+function RREF(a)
+    throw("unimplemented")
+end
+
+function trace(a)
+    throw("unimplemented")
+end
+
+function determinant(a)
+    throw("unimplemented")
+end
+
+function rank(a)
+throw("unimplemented")
+end
+
+function nullality(a)
+    throw("unimplemented")
+end
+
+function eigenVals(a)
+    throw("unimplemented")
+end
+
+function eigenVectors(a)
+    throw("unimplemented")
+end
