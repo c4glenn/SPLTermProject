8b3ef439c1effe8c90949476ac9b3d7531139428
avaldez711 <144289409+avaldez711@users.noreply.github.com>
2023-11-08 14:55:32 -0500

    Update tesseractTest.jl
    added comments for context

--- tesseractTest.jl
+++ tesseractTest.jl
@@ -1,6 +1,8 @@
 using OCReract
 using Images
 
+# For this file to run you need to have tesseract installed https://tesseract-ocr.github.io/tessdoc/Installation.html
+
 img_path = "lightbox.jpg"
 img = load(img_path)
 
