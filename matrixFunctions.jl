5960e261bb0e7e738c7321a4e8d3ac40633f74b2
Nick-Koehler87 <0nicholaskoehler@gmail.com>
2023-11-09 11:31:32 -0500

    worked on the functions
    dimensions works. transpose almost works. The only thing that isn't working is defining the new array.

--- matrixFunctions.jl
+++ matrixFunctions.jl
@@ -1,11 +1,23 @@
 # https://docs.julialang.org/en/v1/stdlib/LinearAlgebra/
 
 function dimensions(a)
-    throw("unimplemented")
+    rows = size(a)[1]
+    cols = size(a[1])[1]
+    return [rows, cols]
 end
 
-function transpose(a)
-    throw("unimplemented")
+function transposer(a)
+    dim = dimensions(a)
+    print(a)
+    #this should all work but when I try to create a 2d array it makes this weird matrix thing. I dont understand whats going on >:(
+    b = ones(Int, dim[2], dim[1])
+    print(b)
+    for i in 1:dim[2]
+        for k in 1:dim[1]
+            b[i][k] = a[k][i]
+        end
+    end
+    print(b)
 end
 
 
@@ -36,3 +48,10 @@ end
 function eigenVectors(a)
     throw("unimplemented")
 end
+
+#testing transposer
+a = [[1,2,3],[4,5,6]]
+
+c = transposer(a)
+
+println(c)
