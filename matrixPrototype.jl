cb3181bc6c0d1696ae7400f57e1316852ec4e7e5
Andrew <avaldez5@uncc.edu>
2023-11-14 18:23:24 -0500

    image updates on screen when user selects image

--- matrixPrototype.jl
+++ matrixPrototype.jl
@@ -16,13 +16,17 @@ choose_file_input = GtkEntry()
 
 process_image_button = GtkButton("Process Image")
 
+selected_image = GtkImage("")
+
 matrix_label = GtkLabel("")
 
 container[1,1] = path_label
 container[2,1] = choose_file_input
 container[3,1] = choose_file_button
 container[2,2] = process_image_button
-container[2,3] = matrix_label
+container[2,3] = selected_image
+container[2,4] = matrix_label
+
 
 set_gtk_property!(choose_file_input, :placeholder_text, "File Not Chosen")
 
@@ -35,13 +39,16 @@ showall(window)
 function choose_file(w)
     file_dialog = open_dialog("Pick an image file", GtkNullContainer(), ("*.png", "*.jpg", GtkFileFilter("*.png, *.jpg", name="All supported formats")))
     set_gtk_property!(choose_file_input,:text, file_dialog)
-    
+    img_path = get_gtk_property(choose_file_input, :text, String)
+    set_gtk_property!(selected_image, :file, img_path)
+
 end
 
 function process_image(w)
     img_path = get_gtk_property(choose_file_input, :text, String)
     img = load(img_path)
     res = run_tesseract(img, psm=6)
+    println(res)
     set_gtk_property!(matrix_label, :label, res)
 end
 signal_connect(choose_file, choose_file_button, "clicked")
